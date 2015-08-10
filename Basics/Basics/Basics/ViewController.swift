a8d06c7a75b8daedc8ed1cae6e3069924f91adbd
Venkata Narasimham Peetla <pvnarasimham1@gmail.com>
2015-08-10 14:04:31 -0700

    initial commit

---
+++ Basics/Basics/Basics/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Basics
+//
+//  Created by PV Narasimham Peetla on 8/7/15.
+//  Copyright (c) 2015 PV Narasimham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
